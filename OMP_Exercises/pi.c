48036a358d0572d9e58e44b18dd96052efb9536d
Kaiyuan Hou <khou2020@outlook.com>
2018-08-01 13:58:56 -0500

    parallel pi use pfor

--- OMP_Exercises/pi.c
+++ OMP_Exercises/pi.c
@@ -28,8 +28,9 @@ double step;
 static long num_steps = 1000000000;
 double step;
 
-int main(int argc, char* argv[]) {
+int main (int argc, char* argv[]) {
 	int nthread = 4;
+	int i;
 	double x, pi, sum = 0.0;
 
 	if (argc > 1) {
@@ -37,32 +38,21 @@ int main(int argc, char* argv[]) {
 	}
 	omp_set_num_threads(nthread);
 
-	step = 1.0 / (double)num_steps;
+	step = 1.0 / (double)num_steps; 
 
 	double tdata = omp_get_wtime();
 
-#pragma omp parallel reduction(+: sum)
-	{
-		int i, s, t, tid, nt;
-		double x, lsum = 0;
-
-		nt = omp_get_num_threads();
-		tid = omp_get_thread_num();
-		s = num_steps / nt + 1;
-		t = s * (tid + 1);
-		s = t - s;
-
-		//printf("%d, %d, %d\n", omp_get_thread_num(), s, t);
-		for (i = s; i < t; i++) {
+#pragma omp parallel
+	{	
+#pragma omp for private(x) reduction(+: sum) schedule(static)
+		for (i = 0; i < num_steps; i++) {
 			x = ((double)i + 0.5)*step;
-			lsum += 4.0 / (1.0 + x*x);
+			sum += 4.0 / (1.0 + x*x);
 		}
-		//#pragma omp critical
-		sum += lsum;
 	}
-	pi = step * sum;
+	pi = step * sum; 
 
-	tdata = omp_get_wtime() - tdata;
+	tdata = omp_get_wtime() - tdata; 
 	printf(" pi = %f in %f secs\n", pi, tdata);
 
 	return 0;
