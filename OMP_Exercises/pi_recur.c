32a6185addea3248b83d670451677ec2b44f77a9
Kaiyuan Hou <khou2020@outlook.com>
2018-08-01 17:25:27 -0500

    pi_recur

--- OMP_Exercises/pi_recur.c
+++ OMP_Exercises/pi_recur.c
@@ -14,37 +14,55 @@ History: Written by Tim Mattson, 10/2013
 
 */
 #include <omp.h>
+#include <stdlib.h>
 #include <stdio.h>
-static long num_steps = 1024*1024*1024;
-#define MIN_BLK  1024*1024*256
+static long num_steps = 1024 * 1024 * 1024;
+#define MIN_BLK 1024 * 1024 * 256
 
-double pi_comp(int Nstart,int Nfinish,double step)
-{  int i,iblk;
-   double x, sum = 0.0,sum1, sum2;
-   if (Nfinish-Nstart < MIN_BLK){
-      for (i=Nstart;i< Nfinish; i++){
-         x = (i+0.5)*step;
-         sum = sum + 4.0/(1.0+x*x); 
-      }
-   }
-   else{
-      iblk = Nfinish-Nstart;
-      sum1 = pi_comp(Nstart,         Nfinish-iblk/2,step);
-      sum2 = pi_comp(Nfinish-iblk/2, Nfinish,       step);
-      sum = sum1 + sum2;
-   }return sum;
+double pi_comp(int Nstart, int Nfinish, double step)
+{
+	int i, iblk;
+	double x, sum = 0.0, sum1, sum2;
+	if (Nfinish - Nstart < MIN_BLK)
+	{
+		for (i = Nstart; i < Nfinish; i++)
+		{
+			x = (i + 0.5) * step;
+			sum = sum + 4.0 / (1.0 + x * x);
+		}
+	}
+	else
+	{
+		iblk = Nfinish - Nstart;
+#pragma omp task shared(sum1)
+		sum1 = pi_comp(Nstart, Nfinish - iblk / 2, step);
+#pragma omp task shared(sum2)
+		sum2 = pi_comp(Nfinish - iblk / 2, Nfinish, step);
+#pragma omp taskwait
+		sum = sum1 + sum2;
+	}
+	return sum;
 }
- int main ()
- {
-   int i;
-   double step, pi, sum;
-   double init_time, final_time;
-   step = 1.0/(double) num_steps;
+int main(int argc, char* argv[])
+{
+	int i;
+	double step, pi, sum;
+	double init_time, final_time;
+	int nthread;
+	step = 1.0 / (double)num_steps;
 
-   init_time = omp_get_wtime();
-   sum = pi_comp(0,num_steps,step);
-   pi = step * sum;
-   final_time = omp_get_wtime() - init_time;
-   printf(" for %ld steps pi = %f in %f secs\n",num_steps,pi,final_time);
-  
- }  
+	if (argc > 1) {
+		nthread = atoi(argv[1]);
+	}
+	omp_set_num_threads(nthread);
+
+	init_time = omp_get_wtime();
+#pragma omp parallel
+	{
+		#pragma omp single
+		sum = pi_comp(0, num_steps, step);
+	}
+	pi = step * sum;
+	final_time = omp_get_wtime() - init_time;
+	printf(" for %ld steps pi = %f in %f secs\n", num_steps, pi, final_time);
+}
