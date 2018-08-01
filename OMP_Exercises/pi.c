83e6727a0f579659424c5d94e937d79f01b96225
Kaiyuan Hou <khou2020@outlook.com>
2018-08-01 13:57:30 -0500

    parallel pi padded

--- OMP_Exercises/pi.c
+++ OMP_Exercises/pi.c
@@ -15,14 +15,18 @@ History: Written by Tim Mattson, 11/99.
 */
 #include <stdio.h>
 #include <omp.h>
+
+#define PAD 16
+
 static long num_steps = 100000000;
 double step;
-int main()
-{
-	int nthread = 4;
-	double x, pi, sum = 0.0;
 
-	if (argc > 1){
+int main(){
+	int nthread = 4;
+	double x, pi;
+	double sum[16][PAD];
+
+	if (argc > 1) {
 		nthread = atoi(argv[1]);
 	}
 
@@ -32,29 +36,38 @@ int main()
 
 	double tdata = omp_get_wtime();
 
-#pragma omp parallel reduction(+ \
-							   : sum)
+#pragma omp parallel
 	{
-		int i, s, t, tid, nt;
-		double x, lsum = 0;
+		int tid, nt;
+		double x;
+		register double lsum = 0;
+		int s, t;
+		register int i;
 
 		nt = omp_get_num_threads();
 		tid = omp_get_thread_num();
+
+		if (tid == 0) {
+			nthread = nt;
+		}
+
 		s = num_steps / nt + 1;
 		t = s * (tid + 1);
 		s = t - s;
 
 		//printf("%d, %d, %d\n", omp_get_thread_num(), s, t);
-		for (i = s; i < t; i++){
+		for (i = s; i < t; i++) {
 			x = ((double)i + 0.5) * step;
 			lsum += 4.0 / (1.0 + x * x);
 		}
 
-		//#pragma omp critical
-		sum += lsum;
+		sum[tid][0] = lsum;
 	}
 
-	pi = step * sum;
+	for (int i = 1; i < nthread; i++) {
+		sum[0][0] += sum[i][0];
+	}
+	pi = step * sum[0][0];
 
 	tdata = omp_get_wtime() - tdata;
 	printf(" pi = %f in %f secs\n", pi, tdata);
